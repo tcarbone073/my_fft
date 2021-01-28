3356ff827d7a38f57e7453e6dc1442ae77c7c210
Tyler Carbone <tcarbone073@gmail.com>
2021-01-28 17:26:36 -0500

    fix semicolon

--- main.m
+++ main.m
@@ -3,5 +3,5 @@ clear;
 close all;
 
 % filename, n_blocks, overlap, win_type, if_plot_subplot, if_plot_autospectra
-fft_data = my_fft('sample_noise2.mp3',[],[],[],false,true)
-;
+fft_data = my_fft('sample_noise2.mp3',[],[],[],false,true);
+
